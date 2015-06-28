e992b6a78f6c3707c50b6f084c3b0f1335cd4a34
gobytony <gobyhl@126.com>
2015-06-29 07:24:25 +0800

    test
    test

--- 02-按钮操作/ViewController.m
+++ 02-按钮操作/ViewController.m
@@ -7,21 +7,59 @@
 //
 
 #import "ViewController.h"
+#define deta 20;
+typedef enum {
+    kMovingDirTop=11,
+    kMovingDirBottom,
+    kMovingDirLeft,
+    kMovingDirRight,
+}kMovingDir;
+
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UIButton *iconButton;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)move:(UIButton *)button {
+    //    1.取出frame
+    CGRect frame = self.iconButton.frame;
+    //    2.修改结构体的成员
+    switch (button.tag) {
+        case kMovingDirTop:
+            frame.origin.y -=deta;
+            break;
+        case kMovingDirBottom:
+            frame.origin.y +=deta;
+            break;
+        case kMovingDirLeft:
+            frame.origin.x -=deta;
+            break;
+        case kMovingDirRight:
+            frame.origin.x +=deta;
+            break;
+            
+        default:
+            break;
+    }
+//    frame.origin.x -=2;
+    self.iconButton.frame =frame;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)top {
+//    self.iconButton.frame.origin.y -=20;
+//    如果修改对象的结构体属性，需要三个步骤
+//    1.取出frame
+    CGRect frame = self.iconButton.frame;
+//    2.修改结构体的成员
+    frame.origin.x -=2;
+//    3.重新为对象的结构体赋值。
+    self.iconButton.frame =frame;
 }
 
 @end
